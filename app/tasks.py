2e6820e4a05bb334916c88e2f7a2138368faaf18
Nikita Shultais <nikita@shultais.ru>
2026-02-26 12:05:55 +0200

    Повторное выполнение заданий

--- app/tasks.py
+++ app/tasks.py
@@ -1,8 +1,19 @@
 from app.celery_app import celery_app
 
 
-@celery_app.task(name="send_message", bind=True)
+@celery_app.task(
+    name="send_message",
+    bind=True,
+    max_retries=None,
+    retry_backoff=True,  # Экспоненциальная задержка,
+    retry_backoff_max=16,
+    # retry_jitter=True,
+    autoretry_for=(ValueError,),
+)
 def send_message(self, message_text: str):
+
+    # raise ValueError("Test error")
+
     return {
         "task_id": self.request.id,
         "processed": message_text.upper()
