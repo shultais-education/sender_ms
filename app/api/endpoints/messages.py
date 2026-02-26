e6fe612b2ec85db07e90cf73cc0d396067f73acb
Nikita Shultais <nikita@shultais.ru>
2026-02-26 11:58:22 +0200

    Перенес задачу в сервис

--- app/api/endpoints/messages.py
+++ app/api/endpoints/messages.py
@@ -3,7 +3,6 @@ from fastapi import status
 
 from app.schemas.message import MessageRequest, TaskResponse
 from app.api.dependencies.messages import MessageServiceDep
-from app.tasks import send_message as send_message_task
 
 
 messages_router = APIRouter(prefix="/messages", tags=["messages"])
@@ -11,15 +10,4 @@ messages_router = APIRouter(prefix="/messages", tags=["messages"])
 
 @messages_router.post("", response_model=TaskResponse, summary="Отправка сообщения", status_code=status.HTTP_202_ACCEPTED)
 async def send_message(message: MessageRequest, message_service: MessageServiceDep):
-    task = await message_service.send_message(message=message.text, delay=message.delay)
-
-    print(message)
-    print(task)
-
-    message_task = send_message_task.apply_async(args=[message.text], countdown=message.delay)
-
-    return TaskResponse(
-        task_id=message_task.task_id,
-        status="PENDING",
-        message="Задача успешно поставлена в очередь"
-    )
+    return await message_service.send_message(message=message.text, delay=message.delay)
--- app/services/messages.py
+++ app/services/messages.py
@@ -1,12 +1,15 @@
 from app.schemas.message import TaskResponse
+from app.tasks import send_message as send_message_task
 
 
 class MessageService:
 
     @staticmethod
     async def send_message(message: str, delay: int = 0) -> TaskResponse:
+        task = send_message_task.apply_async(args=[message], countdown=delay)
+
         return TaskResponse(
-            task_id="1",
-            status="статус",
-            message=message
+            task_id=task.task_id,
+            status="PENDING",
+            message="Задача успешно поставлена в очередь"
         )
